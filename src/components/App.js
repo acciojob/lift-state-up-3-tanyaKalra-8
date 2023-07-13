9c8732a7d3220bba224e9bf4029f80e94864e432
Taniya <tanya.kalra200@gmail.com>
2023-07-13 17:39:01 +0530

    submitting code-Thu Jul 13 2023 17:38:57 GMT+0530 (India Standard Time)

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,34 @@
-
-import React from "react";
-import './../styles/App.css';
+import React, {useState} from "react";
+import "./../styles/App.css";
 
 const App = () => {
+  const [text, setText] = useState("");
+
+  function clicked(event){
+    setText("Option 1");
+  }
+  function clicked1(event){
+    setText("Option 2");
+  }
+
   return (
     <div>
-        {/* Do not remove the main div */}
-    </div>
-  )
-}
+      {/* Do not remove the main div */}
+      <div className="parent">
+        <h1>Parent Component</h1>
 
-export default App
+        <div className="child1">
+          <h1>Child Component 1</h1>
+          <button onClick={clicked}>Option 1</button>
+        </div>
+        <div className="child2">
+          <h1>Child Component 2</h1>
+          <button onClick={clicked1}>Option 2</button>
+        </div>
+        <p>{"Selected Option: " + text}</p>
+      </div>
+    </div>
+  );
+};
+
+export default App;
